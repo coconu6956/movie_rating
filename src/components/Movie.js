c5efc611436dfa171f819b5e6f70fd4e517fcef6
coconu6956 <wce0422@gmail.com>
2022-05-09 17:05:11 +0900

    movie detail 리다이렉트

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
-import './Movie.css'
+import './Movie.css';
+import { Link } from "react-router-dom";
 
-function Movie({title, year, summary, poster}){
+function Movie({title, year, summary, poster, genres}){
     return (
         <div className="movie">
+            <Link to={{
+                pathname: '/movie-detail',
+                state: {year, title, summary, poster, genres },
+            }}>    
+            <img src={poster} alt={title} title={title} />
             <div className="movie__data">
-                <img src={poster} alt={title} title={title} />
                 <h3 className="movie__title">{ title }</h3>
                 <h5 className="movie__year">{year}</h5>
-                <p className="movie__summary">{summary}</p>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                       <li key={index} className="genres__genre">{genre}</li> 
+                        ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0,180)}</p>
             </div>
+            </Link>
         </div>
     );
 }
@@ -20,6 +31,7 @@ Movie.propTypes= {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
